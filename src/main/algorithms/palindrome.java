960153ad3d6a05b4b3eb4b6687562b0942a8865a
zoey <kexin.ma2@mail.dcu.ie>
2025-10-04 18:07:57 +0100

    palindrome

--- src/main/algorithms/palindrome.java
+++ src/main/algorithms/palindrome.java
@@ -21,4 +21,58 @@ class Solution {
     }
 }
 
-//
+//https://leetcode.com/problems/valid-palindrome-ii/
+class Solution {
+    public boolean validPalindrome(String s) {
+        char[] c = s.toCharArray();
+        int i=0;
+        int j=c.length-1;
+        while(i<j){
+            if(c[i]!=c[j]){
+                return validCharPalindrome(c,i+1,j)||validCharPalindrome(c,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validCharPalindrome(char[] s,int left,int right){
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+//https://leetcode.com/problems/longest-palindrome/
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        char[] c = s.toCharArray();
+        int max=0;
+        boolean hasOdd = false;
+        for (int i=0;i<c.length;i++){
+            count[c[i]]++;
+        }
+        for(int j=0;j<count.length;j++){
+            if(count[j]>0){
+                if(count[j]%2==0){
+                    max+=count[j];
+                }else if(count[j]==1){
+                    if(!hasOdd){
+                        max+=1;
+                        hasOdd = true;
+                    }
+                }else{
+                    int tem=count[j]-1;
+                    max+=tem;
+                }
+            }
+        }
+        return max;
+    }
+}
